fc18d65295a04362291124addd3a025df137eea0
mltlima <miguellt@dcc.ufrj.br>
2022-07-26 20:23:24 -0300

    factories

---
+++ back-end/tests/factories/bodyFactory.ts
@@ -0,0 +1,18 @@
+export default function bodyFactory() {
+    const recomendations = [
+        {
+          name: "Leandro e Leonardo - Não Aprendi a dizer Adeus",
+          youtubeLink: "https://www.youtube.com/watch?v=DO9Y2E_tMYY",
+        },
+        {
+          name: "Leandro e Leonardo - Pense em Mim",
+          youtubeLink: "https://www.youtube.com/watch?v=IKHsLKXGjTU",
+        },
+        {
+          name: "João Mineiro e Marciano - Seu Amor Ainda É Tudo",
+          youtubeLink:
+            "https://www.youtube.com/watch?v=aGuN_WuuSzs",
+          score: 112,
+        },
+    ]
+}
---
+++ back-end/tests/factories/recomendationsFactory.ts
@@ -0,0 +1,24 @@
+export default function recomendationsFactory() {
+    const recommendations = [
+      {
+        id: 1,
+        name: "Chitãozinho E Xororó - Evidências",
+        youtubeLink: "https://www.youtube.com/watch?v=ePjtnSPFWK8&ab_channel=CHXVEVO",
+        score: 22,
+      },
+      {
+        id: 2,
+        name: "Milionário e José Rico - Boate Azul",
+        youtubeLink: "https://www.youtube.com/watch?v=77LfH30UJ_g",
+        score: 11,
+      },
+      {
+        id: 3,
+        name: "João Moreno e Mariano - Ainda Ontem Chorei de Saudade",
+        youtubeLink: "https://www.youtube.com/watch?v=0WpfvN2JYdU",
+        score: -10,
+      },
+    ];
+  
+    return recommendations;
+  }
